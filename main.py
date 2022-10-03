db742a79ebd2583493b8c35e57b5e1e11d370ef7
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-10-03 19:54:08 +0000

    loops practice

--- main.py
+++ main.py
@@ -9,9 +9,29 @@
 #importing modules
 #work on creating and understanding functions
 
-from decision_making import decisions
-from comparison_operators import comparison
+# from decision_making import decisions
+# from comparison_operators import comparison
+# from dictionaries_loop import dictionaries_loop
+# from enumerators import enumerators
+# from list_comprehension import list_comprehension
+# from logical_operators import logical_operators
+from loops import loop
+# from min_max import min_max
+# from random import random
+# from ranges import ranges
+# from while_loop import while_loop
+# from zip import zip
 
-decisions()
-comparison()
 
+# decisions()
+# comparison()
+# dictionaries_loop()
+# enumerators()
+# list_comprehension()
+# logical_operators()
+loop()
+# min_max()
+# random()
+# ranges()
+# while_loop()
+# zip()
