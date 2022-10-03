f310acde2d2c0af3275c1f2afa32c07e9c3ada92
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-10-03 20:02:42 +0000

    for loops practice 3

--- loops.py
+++ loops.py
@@ -38,7 +38,7 @@ def loop():
   # For Loops Practice #3
   # Given the following list of numbers, perform the sum of all even and odd* numbers separately in the variables sum_even and sum_odd respectively:
   
-  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+    list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
   
   # *Recall from previous days: the modulus (or remainder) of a number divided by 2 is zero when said value is even, and 1 when it is odd
   
@@ -52,12 +52,12 @@ def loop():
   
   sum_odd = 0
 
-    for number in list_numbers:
+  for number in list_numbers:
     if number % 2 == 0:
       sum_even = sum_even +number
       print(f"evens are {sum_even}")
     else:
       sum_odd = sum_odd + number
-      print(f"evens are {sum_odd}")
+      print(f"odds are {sum_odd}")
 
   
